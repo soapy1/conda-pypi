178914081d678f012279a58bd2f88212ddc1c8a3
sophia <scastellarin@quansight.com>
2025-09-18 10:34:45 -0700

    Run install of conda packages

--- conda_pypi/cli/install.py
+++ conda_pypi/cli/install.py
@@ -6,6 +6,7 @@ from conda.base.context import context
 from conda.models.match_spec import MatchSpec
 
 from conda_pypi import convert_tree
+from conda_pypi.main import run_conda_install
 
 
 def configure_parser(parser: _SubParsersAction) -> None:
@@ -71,5 +72,9 @@ def execute(args: Namespace) -> int:
     # Convert package strings to MatchSpec objects
     match_specs = [MatchSpec(pkg) for pkg in args.packages]
     converter.convert_tree(match_specs)
+    channel_url = converter.repo.as_uri()
+
+    # Install converted packages to current conda environment
+    run_conda_install(prefix_path, match_specs, channels=[channel_url], override_channels=False)
 
     return 0
